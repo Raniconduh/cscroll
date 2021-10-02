bcf16bc05da529b2c7d6b48d930b5473cf0a4333
Raniconduh <clagv.randomgames@gmail.com>
2021-10-01 20:28:24 -0500

    fixed issue with colors

--- include/io.h
+++ include/io.h
@@ -5,20 +5,20 @@
 #include <stdbool.h>
 
 enum colors {
-	BLUE,
-	HBLUE,
-	CYAN,
-	HCYAN,
-	GREEN,
-	HGREEN,
-	MAGENTA,
-	HMAGENTA,
-	YELLOW,
-	HYELLOW,
-	RED,
-	HRED,
-	WHITE,
-	HWHITE,
+	BLUE = 1,
+	HBLUE = 2,
+	CYAN = 3,
+	HCYAN = 4,
+	GREEN = 5,
+	HGREEN = 6,
+	MAGENTA = 7,
+	HMAGENTA = 8,
+	YELLOW = 9,
+	HYELLOW = 10,
+	RED = 11,
+	HRED = 12,
+	WHITE = 13,
+	HWHITE = 14,
 };
 
 enum keys {
