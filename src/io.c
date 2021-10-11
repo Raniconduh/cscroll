8b33f32bdc4e620857ecea0ca69b8d78087c38ab
Raniconduh <clagv.randomgames@gmail.com>
2021-10-10 21:42:26 -0500

    removed unnecesary code; more efficient file name printing

--- src/io.c
+++ src/io.c
@@ -21,19 +21,12 @@ void curses_init(void) {
 	start_color();
 	
 	init_pair(BLUE, COLOR_BLUE, COLOR_BLACK);
-    init_pair(HBLUE, COLOR_BLACK, COLOR_BLUE);
-    init_pair(CYAN, COLOR_CYAN, COLOR_BLACK);
-    init_pair(HCYAN, COLOR_BLACK, COLOR_CYAN);
-    init_pair(GREEN, COLOR_GREEN, COLOR_BLACK);
-    init_pair(HGREEN, COLOR_BLACK, COLOR_GREEN);
-    init_pair(MAGENTA, COLOR_MAGENTA, COLOR_BLACK);
-    init_pair(HMAGENTA, COLOR_BLACK, COLOR_MAGENTA);
-    init_pair(YELLOW, COLOR_YELLOW, COLOR_BLACK);
-    init_pair(HYELLOW, COLOR_BLACK, COLOR_YELLOW);
-    init_pair(RED, COLOR_RED, COLOR_BLACK);
-    init_pair(HRED, COLOR_BLACK, COLOR_RED);
-    init_pair(WHITE, COLOR_WHITE, COLOR_BLACK);
-    init_pair(HWHITE, COLOR_BLACK, COLOR_WHITE); 
+	init_pair(CYAN, COLOR_CYAN, COLOR_BLACK);
+	init_pair(GREEN, COLOR_GREEN, COLOR_BLACK);
+	init_pair(MAGENTA, COLOR_MAGENTA, COLOR_BLACK);
+	init_pair(YELLOW, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(RED, COLOR_RED, COLOR_BLACK);
+	init_pair(WHITE, COLOR_WHITE, COLOR_BLACK);
 }
 
 
@@ -51,87 +44,52 @@ void terminate_curses(void) {
 
 void curses_write_file(struct dir_entry_t * dir_entry, bool highlight) {
 	int cp = -1;
-	
 	char f_ident;
-	if (highlight) {
-		switch (dir_entry->file_type) {
-			case FILE_DIR:
-				cp = HBLUE;
-				f_ident = '/';
-				break;
-			case FILE_FIFO:
-				cp = HYELLOW;
-				f_ident = '|';
-				break;
-			case FILE_BLK:
-				cp = HYELLOW;
-				f_ident = '#';
-				break;
-			case FILE_LINK:
-				cp = HCYAN;
-				f_ident = '@';
-				break;
-			case FILE_SOCK:
-				cp = HMAGENTA;
-				f_ident = '=';
-				break;
-			case FILE_UNKNOWN:
-				cp = HRED;
-				f_ident = '?';
-				break;
-			default:
-				f_ident = NO_IDENT;
-				break;
-		}
-		if (dir_entry->exec) {
-			cp = HGREEN;
-			if (f_ident == NO_IDENT) f_ident = '*';
-		} else if (cp == -1) {
-			cp = HWHITE;
-			f_ident = ' ';
-		}
-	} else {
-		switch (dir_entry->file_type) {
-			case FILE_DIR:
-				cp = BLUE;
-				f_ident = '/';
-				break;
-			case FILE_FIFO:
-				cp = YELLOW;
-				f_ident = '|';
-				break;
-			case FILE_BLK:
-				cp = YELLOW;
-				f_ident = '#';
-				break;
-			case FILE_LINK:
-				cp = CYAN;
-				f_ident = '@';
-				break;
-			case FILE_SOCK:
-				cp = MAGENTA;
-				f_ident = '=';
-				break;
-			case FILE_UNKNOWN:
-				cp = RED;
-				f_ident = '?';
-				break;
-			default:
-				f_ident = NO_IDENT;
-				break;
-		}
-		if (dir_entry->exec) {
-			cp = GREEN;
-			if (f_ident == NO_IDENT) f_ident = '*';
-		} else if (cp == -1) {
-			cp = WHITE;
-			f_ident = ' ';
-		}
+
+	switch (dir_entry->file_type) {
+		case FILE_DIR:
+			cp = BLUE;
+			f_ident = '/';
+			break;
+		case FILE_FIFO:
+			cp = YELLOW;
+			f_ident = '|';
+			break;
+		case FILE_BLK:
+			cp = YELLOW;
+			f_ident = '#';
+			break;
+		case FILE_LINK:
+			cp = CYAN;
+			f_ident = '@';
+			break;
+		case FILE_SOCK:
+			cp = MAGENTA;
+			f_ident = '=';
+			break;
+		case FILE_UNKNOWN:
+			cp = RED;
+			f_ident = '?';
+			break;
+		default:
+			f_ident = NO_IDENT;
+			break;
 	}
 
-	attron(COLOR_PAIR((unsigned)cp));
+	if (dir_entry->exec) {
+		cp = GREEN;
+		if (f_ident == NO_IDENT) f_ident = '*';
+	} else if (cp == -1) {
+		cp = WHITE;
+		f_ident = ' ';
+	}
+	
+	cp = COLOR_PAIR((unsigned)cp);
+	if (highlight) cp |= A_REVERSE;
+
+	attron(cp);
 	printw("%s", dir_entry->name);
-	attroff(COLOR_PAIR((unsigned)cp));
+	attroff(cp);
 	printw("%c\n", f_ident);
 }
 
