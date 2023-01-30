263b238896c493562e94fbd09d9c5a8deac06172
Raniconduh <clagv.randomgames@gmail.com>
2023-01-30 07:45:00 -0600

    allow ICONS to be overwritten as env variable, add POSIX.1-2001 macro definition

--- config.mk
+++ config.mk
@@ -1,6 +1,7 @@
 PREFIX = /usr/local
 
-ICONS = 1
-CFLAGS += -Iinclude -Wall -Wextra -pedantic -DICONS=${ICONS}
+ICONS ?= 1
+CFLAGS += -Iinclude -Wall -Wextra -pedantic -D_POSIX_C_SOURCE=200112L \
+		  -DICONS=${ICONS}
 
 LDFLAGS = `pkg-config --libs ncursesw || pkg-config --libs ncurses`
