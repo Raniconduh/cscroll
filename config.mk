bb041e7020fafb7e24511f1d01bc55eb98ba52a3
minzique <minzique@protonmail.com>
2023-01-30 13:26:12 +0530

    removed cursed shit and made it work

--- config.mk
+++ config.mk
@@ -3,6 +3,4 @@ PREFIX = /usr/local
 ICONS = 1
 CFLAGS += -Iinclude -Wall -Wextra -pedantic -DICONS=${ICONS}
 
-NCURSES = ncurses`pkg-config --exists ncursesw && echo w`
-TINFO = `[ "$$(uname)" = Darwin ] && echo -ltinfo`
-LDFLAGS = -l${NCURSES} ${TINFO}
+LDFLAGS = `pkg-config --libs-only-l ncurses`
