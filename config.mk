fc06e0e082a302bc146f365b5ba3eca4c4c529b1
minzique <minzique@protonmail.com>
2023-01-30 19:07:35 +0530

    more cleaner this way

--- config.mk
+++ config.mk
@@ -3,4 +3,4 @@ PREFIX = /usr/local
 ICONS = 1
 CFLAGS += -Iinclude -Wall -Wextra -pedantic -DICONS=${ICONS}
 
-LDFLAGS = `pkg-config --exists ncursesw && pkg-config --libs ncursesw || pkg-config --libs ncurses`
+LDFLAGS = `pkg-config --libs ncursesw || pkg-config --libs ncurses`
